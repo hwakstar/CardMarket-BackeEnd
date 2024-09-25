05787c905d15f834a1ec13e5340f04f09707be67
mythofmidas <handsofmidas007@gmail.com>
2024-09-24 20:27:05 -0400

    Change error function

--- src/affiliate/controllers/Auth/Register.js
+++ src/affiliate/controllers/Auth/Register.js
@@ -7,31 +7,31 @@ const Users = require("../../models/UsersModel");
 const Register = expressAsyncHandler(async (req, res) => {
   const { fullName, email, password, phoneNumber, country } = req.body;
 
-  if (!fullName || !email || !password || !phoneNumber || !country) {
-    throw new Error("Please enter your credentials");
-  }
-
-  const checkMail = await Users.findOne({ email });
-  if (checkMail) {
-    throw new Error("Email already exists, try with a different one");
-  }
-
   try {
-    const user = await Users.create({
-      fullName,
-      email,
-      password,
-      phoneNumber,
-      country,
-    });
+    const checkMail = await Users.findOne({ email });
+    if (checkMail) {
+      res.json({
+        status: false,
+        message: "Email already exists. Try with a different one.",
+      });
+    } else {
+      const user = await Users.create({
+        fullName,
+        email,
+        password,
+        phoneNumber,
+        country,
+      });
 
-    let token = getToken(user.id);
+      const token = getToken({ user_id: user.id, fullName: user.fullName });
 
-    res.json({
-      token,
-      message: "Registration successful",
-      id: user.affiliateId,
-    });
+      res.json({
+        status: true,
+        token,
+        message: "Registration successful",
+        id: user.affiliateId,
+      });
+    }
   } catch (error) {
     res.json({ error, message: "Registration unsuccessful" });
   }
