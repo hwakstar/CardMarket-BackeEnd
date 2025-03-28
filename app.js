aa5979957fd7047be2e2fab0b3fafbba61d86c2b
hwakstar <hwakstar79@gmail.com>
2025-03-28 11:40:55 -0700

    update image upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // // Load SSL certificate and key
 // const options = {
