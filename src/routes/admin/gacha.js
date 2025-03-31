07fb9d5e6a9331b7c9258a579dbcc2b2037b03e8
hwakstar <hwakstar79@gmail.com>
2025-03-31 05:49:35 -0700

    update gacha

--- src/routes/admin/gacha.js
+++ src/routes/admin/gacha.js
@@ -511,6 +511,7 @@ router.post("/draw_gacha", auth, async (req, res) => {
       if (!random_n_p) break;
       let r_n = Math.floor(Math.random() * (un_random_prizes.length - 1));
       let r_n_el = un_random_prizes[r_n];
+      console.log(r_n_el);
       let video = await PrizeVideo.findOne({ kind: r_n_el.kind });
       r_n_el["video"] = video.url;
       res_data.push(r_n_el);
