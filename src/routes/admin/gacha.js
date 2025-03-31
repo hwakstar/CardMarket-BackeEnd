fe342c755656ec9a458730d47bf082bd217ce785
hwakstar <hwakstar79@gmail.com>
2025-03-31 08:33:41 -0700

    gacha

--- src/routes/admin/gacha.js
+++ src/routes/admin/gacha.js
@@ -458,12 +458,11 @@ router.post("/draw_gacha", auth, async (req, res) => {
       },
     });
 
-    console.log(target_prizes);
 
     console.log("remove_number ", gacha.remove_number);
     console.log("counts: ", counts);
-    console.log(target_prizes.length);
-    console.log(target_rubbishes.length);
+    console.log("traget_prizes_length: ", target_prizes.length);
+    console.log("target_rubbishes_length: ", target_rubbishes.length);
 
     let random_number = counts - target_prizes.length - target_rubbishes.length;
     console.log("random_number: ", random_number);
@@ -525,10 +524,11 @@ router.post("/draw_gacha", auth, async (req, res) => {
     while (1) {
       if (!random_n_p) break;
 
-      console.log(random_n_p);
-      console.log(un_random_prizes.length);
+      console.log("random_n_p in radom_prizes ", random_n_p);
       let r_n = Math.floor(Math.random() * un_random_prizes.length);
       let r_n_el = un_random_prizes[r_n];
+      console.log("=======r_n_el =======");
+      console.log(r_n_el);
       let video = await PrizeVideo.findOne({ kind: r_n_el.kind });
       r_n_el["video"] = video.url;
       res_data.push(r_n_el);
