0e2121af48550505a02a80071bcb51ddf88e1d0b
hwakstar <hwakstar79@gmail.com>
2025-04-23 06:51:00 -0800

    upate modal

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String },
     hashedPass: { type: String },
     address: { type: String },
     city: { type: String },
