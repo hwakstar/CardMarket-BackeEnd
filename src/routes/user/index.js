8de0fcaca4986aa34b856d5932d01284f39b0829
hwakstar <hwakstar79@gmail.com>
2025-04-22 20:32:55 -0800

    open SNS

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -548,7 +548,7 @@ router.post("/sns", async (req, res) => {
   verificationCodes.set(phonenumber, { code, expiresAt });
 
   console.log("📧 Message: ", message);
-  // await sendSms(phonenumber, message);
+  await sendSms(phonenumber, message);
   res.send({ status: 1 });
 });
 
